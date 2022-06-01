20534a62c2aeb5b49748c638cad132eff5499330
caty <1120873075@qq.com>
2022-06-01 18:02:51 +0800

    滑动删除

---
+++ pages/2.6_2.7/index.js
@@ -0,0 +1,86 @@
+// pages/2.6_2.7/index.js
+Page({
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    x: 0,
+    y: 0,
+    scale: 2,
+    // 当前x的值
+    currentX: 0
+  },
+
+  onMovableViewChange(e){
+    console.log("change",e.detail)
+  },
+
+  tap(e) {
+    let kind =  parseInt(e.currentTarget.dataset.kind)
+    if (!kind){
+      this.setData({
+        x: 30,
+        y: 30
+      })
+    }else{
+      this.setData({
+        x: 0,
+        y: 0
+      })
+    }
+  },
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad() {
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload() {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh() {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom() {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage() {
+
+  }
+})
--- pages/index/index.js
+++ pages/index/index.js
@@ -5,7 +5,8 @@ Page({
       {index:"/pages/2.1/index",name:"icon组件：关于图标的4个实现方案"},
       {index:"/pages/2.2/index",name:"progress组件：如何自定义实现一个环形进度条？"},
       {index:"/pages/2.3/index",name:"rich-text组件：如何单击预览rich-text中的图片并保存？"},
-      {index:"/pages/2.4_2.5/index",name:"view容器组件及Flex布局"}
+      {index:"/pages/2.4_2.5/index",name:"view容器组件及Flex布局"},
+      {index:"/pages/2.6_2.7/index",name:"可移动容器及可移动区域"}
     ]
   }
 })
