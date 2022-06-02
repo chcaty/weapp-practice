132bf69736c22b1b0055048f96bd83bc15594d00
caty <1120873075@qq.com>
2022-06-02 22:43:34 +0800

    滑动选择器

--- pages/2.6_2.7/index.js
+++ pages/2.6_2.7/index.js
@@ -1,4 +1,5 @@
 // pages/2.6_2.7/index.js
+var base64 = require("../../images/base64");
 Page({
   /**
    * 页面的初始数据
@@ -33,54 +34,22 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad() {
-  },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady() {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow() {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide() {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload() {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh() {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom() {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage() {
-
+    this.widget = this.selectComponent('.widget')
+    this.setData({
+        icon: base64.icon20,
+        slideButtons: [{
+          text: '普通1',
+          src: '/images/icon_love.svg', // icon的路径
+        },{
+          text: '普通2',
+          extClass: 'test',
+          src: '/images/icon_star.svg', // icon的路径
+        },{
+          type: 'warn',
+          text: '警示3',
+          extClass: 'test',
+            src: '/images/icon_del.svg', // icon的路径
+        }],
+    });
   }
 })
--- pages/index/index.js
+++ pages/index/index.js
@@ -6,7 +6,10 @@ Page({
       {index:"/pages/2.2/index",name:"progress组件：如何自定义实现一个环形进度条？"},
       {index:"/pages/2.3/index",name:"rich-text组件：如何单击预览rich-text中的图片并保存？"},
       {index:"/pages/2.4_2.5/index",name:"view容器组件及Flex布局"},
-      {index:"/pages/2.6_2.7/index",name:"可移动容器及可移动区域"}
+      {index:"/pages/2.6_2.7/index",name:"可移动容器及可移动区域"},
+      {index:"/pages/2.6_2.7/render-wxml/index",name:"weui-canvas"},
+      {index:"/pages/2.8_2.9/index",name:"scroll-view"},
+      {index:"/pages/2.10/index",name:"滚动选择器"},
     ]
   }
 })
