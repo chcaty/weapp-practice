80065d1c252886c1457f0d2e33dc23d73522cd94
caty <1120873075@qq.com>
2022-06-08 18:26:19 +0800

    图片mode

--- pages/2.14/index.js
+++ pages/2.14/index.js
@@ -1,11 +1,37 @@
 const app = getApp()
 
 Page({
+
+  /**
+   * 页面的初始数据
+   */
   data: {
-
+    appear: false,
+    qrcode: 'https://cdn.nlark.com/yuque/0/2020/jpeg/1252071/1590847767698-f511e86d-f183-4f75-a04d-1b99cd9f0bd7.jpeg'
   },
-
   onLoad: function () {
+    // 直接加载短链接网址，返回403
+    // 403代表资源不可用
+    wx.downloadFile({
+      url: 'http://t.cn/A622upBw',
+      complete(res) {
+        console.log(res);
+
+      }
+    })
+
+    // IntersectionObserver示例
+    this._observer = wx.createIntersectionObserver(this)
+    this._observer
+      .relativeTo('.scroll-view')
+      .observe('.ball', (res) => {
+        this.setData({
+          appear: res.intersectionRatio > 0
+        })
+      })
+
+    // ------------
+    // 加载图片数据
     wx.request({
       url: 'https://wxapi.kkgoo.cn/live/discover?type=hot',
       method: 'POST',
@@ -29,5 +55,9 @@ Page({
       current: url,
       urls: [url],
     })
-  }
+  },
+  onUnload() {
+    if (this._observer) this._observer.disconnect()
+  },
+
 })
