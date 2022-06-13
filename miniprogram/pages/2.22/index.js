ef194d2c46d25fa5abed8ecc119c97447c8190ee
caty <1120873075@qq.com>
2022-06-13 14:42:33 +0800

    web-view相关完结

--- miniprogram/pages/2.22/index.js
+++ miniprogram/pages/2.22/index.js
@@ -8,45 +8,107 @@ Page({
 
   },
 
+  onShareAppMessage: function (options) {
+    console.log('分享')
+    return {
+      title: '登陆',
+      path: '/pages/2.22/index'
+    }
+  },
+
   login(e) {
+    console.log(e);
+    let {
+      userInfo,
+      encryptedData,
+      iv
+    } = e.detail
+    console.log('userInfo', userInfo);
+    const requestLoginApi = (code) => {
+      //发起网络请求
+      wx.request({
+        url: 'http://localhost:30000/user/wexin-login2',
+        method: 'POST',
+        header: {
+          'content-type': 'application/json'
+        },
+        data: {
+          code: code,
+          userInfo,
+          encryptedData,
+          iv
+        },
+        success(res) {
+          console.log('请求成功', res.data)
+          let token = res.data.data.authorizationToken
+          wx.setStorageSync('token', token)
+          onUserLogin(token)
+          console.log('authorization', token)
+        },
+        fail(err) {
+          console.log('请求异常', err)
+        }
+      })
+    }
+
+    const onUserLogin = (token) => {
+      getApp().globalData.token = token
+      wx.showToast({
+        title: '登陆成功了',
+      })
+    }
+
+    wx.checkSession({
+      success() {
+        //session_key 未过期，并且在本生命周期一直有效
+        console.log('在登陆中');
+        let token = wx.getStorageSync('token')
+        if (token) onUserLogin(token)
+      },
+      fail() {
+        // session_key 已经失效，需要重新执行登录流程
+        wx.login({
+          success(res0) {
+            if (res0.code) {
+              requestLoginApi(res0.code)
+            } else {
+              console.log('登录失败！' + res.errMsg)
+            }
+          }
+        })
+      }
+    })
+  },
+
+  // 旧登陆方法
+  login2(e) {
+    console.log(e);
+    let { userInfo,
+      encryptedData,
+      iv } = e.detail
     wx.login({
       success(res0) {
         if (res0.code) {
-
-          wx.getUserInfo({
-            withCredentials: true,
-            success: function (res) {
-
-              var { userInfo, encryptedData, iv } = res
-
-              // console.log(userInfo,encryptedData,iv);
-              //发起网络请求
-              wx.request({
-                url: 'http://localhost:30000/user/wexin-login2',
-                method: 'POST',
-                header: {
-                  'content-type': 'application/json'
-                },
-                data: {
-                  code: res0.code,
-                  userInfo,
-                  encryptedData,
-                  iv
-                },
-                success(res) {
-                  console.log('请求成功', res.data)
-                  let token = res.data.data.authorizationToken
-                  getApp().globalData.token = token
-                  wx.showToast({
-                    title: '登陆成功了',
-                  })
-                  console.log('authorization', token)
-                },
-                fail(err) {
-                  console.log('请求异常', err)
-                }
-              })
-
+          //发起网络请求
+          wx.request({
+            url: 'http://localhost:30000/user/wexin-login2',
+            method: 'POST',
+            header: {
+              'content-type': 'application/json'
+            },
+            data: {
+              code: res0.code,
+              userInfo,
+              encryptedData,
+              iv
+            },
+            success(res) {
+              console.log('请求成功', res.data)
+              getApp().globalData.token = res.data.data.authorizationToken
+              console.log('authorization', getApp().globalData.token)
+            },
+            fail(err) {
+              console.log('请求异常', err)
             }
           })
 
@@ -62,7 +124,9 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    // wx.showShareMenu({
+    //   withShareTicket: true
+    // })
   },
 
 
@@ -108,10 +172,11 @@ Page({
 
   },
 
+  // 重复的函数，会重写，导致不易发现的bug
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage: function () {
+  // onShareAppMessage: function () {
 
-  }
+  // }
 })
--- miniprogram/pages/2.22/web-view/index.js
+++ miniprogram/pages/2.22/web-view/index.js
@@ -5,24 +5,24 @@ Page({
    * 页面的初始数据
    */
   data: {
-    url:'',
-    webViewData:{}
+    url: '',
+    webViewData: {}
   },
 
-onReceivedMessage(e){
-  let data = e.detail.data
-  // data可能是多次 postMessage 的参数组成的数组
-  if (Array.isArray(data)) data = data[0]
-  console.log('onReceivedMessage',JSON.parse(data));
-  this.setData({
-    webViewData:JSON.parse(data)
-  })
-},
+  onReceivedMessage(e) {
+    let data = e.detail.data
+    // data可能是多次 postMessage 的参数组成的数组
+    if (Array.isArray(data)) data = data[0]
+    console.log('onReceivedMessage', JSON.parse(data));
+    this.setData({
+      webViewData: JSON.parse(data)
+    })
+  },
 
   onShareAppMessage(options) {
-    console.log('title',this.data.webViewData.title);
-    
-    console.log('webViewUrl',options.webViewUrl)
+    console.log('title', this.data.webViewData.title);
+
+    console.log('webViewUrl', options.webViewUrl)
     return {
       title: this.data.webViewData.title,
       path: `/pages/2.22/web-view/index?web-view-url=${options.webViewUrl}`
