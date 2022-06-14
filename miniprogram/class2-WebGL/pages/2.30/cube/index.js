f287890938ff5174c4457e34fc400130cbad0fc0
caty <1120873075@qq.com>
2022-06-14 15:18:01 +0800

    补充缺少的文件

--- miniprogram/class2-WebGL/pages/2.30/cube/index.js
+++ miniprogram/class2-WebGL/pages/2.30/cube/index.js
@@ -1,4 +1,6 @@
 // pages/2.30/cube/index.js
+import { createScopedThreejs } from 'threejs-miniprogram'
+import { OrbitControls } from '../../../jsm/controls/OrbitControls'
 Page({
 
   /**
@@ -11,8 +13,90 @@ Page({
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad(options) {
+  onLoad: function () {
+    wx.createSelectorQuery()
+      .select('#c')
+      .node()
+      .exec((res) => {
+        let canvasId = res[0].node._canvasId
+        const canvas = res[0].node
+        this.canvas = canvas
 
+        const THREE = createScopedThreejs(canvas)
+        // console.log(THREE);
+
+        // THREE.global.registerCanvas(canvasId, canvas)
+
+        this.setData({ canvasId })
+
+        const camera = new THREE.PerspectiveCamera(70, canvas.width / canvas.height, 1, 1000);
+        camera.position.z = 500;
+        const scene = new THREE.Scene();
+        scene.background = new THREE.Color(0xAAAAAA);
+        const renderer = new THREE.WebGLRenderer({ antialias: true });
+
+        const controls = new OrbitControls(camera, renderer.domElement);
+        // controls.enableDamping = true;
+        // controls.dampingFactor = 0.25;
+        // controls.enableZoom = false;
+        camera.position.set(200, 200, 500);
+        controls.update();
+        const geometry = new THREE.BoxBufferGeometry(200, 200, 200);
+
+        const texture = new THREE.TextureLoader().load('./pikachu.png');
+        const material = new THREE.MeshBasicMaterial({ map: texture });
+
+        // const material = new THREE.MeshBasicMaterial({ color: 0x44aa88 });
+        const mesh = new THREE.Mesh(geometry, material);
+        scene.add(mesh);
+
+        // renderer.setPixelRatio(wx.getSystemInfoSync().pixelRatio);
+        // renderer.setSize(canvas.width, canvas.height);
+
+        function onWindowResize() {
+          camera.aspect = window.innerWidth / window.innerHeight;
+          camera.updateProjectionMatrix();
+          renderer.setSize(canvas.width, canvas.height);
+        }
+        function render() {
+          canvas.requestAnimationFrame(render);
+          // mesh.rotation.x += 0.005;
+          // mesh.rotation.y += 0.01;
+          controls.update();
+          renderer.render(scene, camera);
+        }
+
+        render()
+
+      })
+  },
+  // 代替原来的传递方法
+  touchStart(e) {
+    this.canvas.dispatchTouchEvent({ ...e, type: 'touchstart' })
+  },
+  touchMove(e) {
+    this.canvas.dispatchTouchEvent({ ...e, type: 'touchmove' })
+  },
+  touchEnd(e) {
+    this.canvas.dispatchTouchEvent({ ...e, type: 'touchend' })
+  },
+  touchCancel(e) {
+    // console.log('canvas', e)
+  },
+  longTap(e) {
+    // console.log('canvas', e)
+  },
+  tap(e) {
+    // console.log('canvas', e)
+  },
+  documentTouchStart(e) {
+    // console.log('document',e)
+  },
+  documentTouchMove(e) {
+    // console.log('document',e)
+  },
+  documentTouchEnd(e) {
+    // console.log('document',e)
   },
 
   /**
