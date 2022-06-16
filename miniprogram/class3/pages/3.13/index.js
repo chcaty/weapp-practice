9dc4f9aa8b8c408c900541aab2ec47606224ff06
caty <1120873075@qq.com>
2022-06-16 18:02:32 +0800

    Page扩展

---
+++ miniprogram/class3/pages/3.13/index.js
@@ -0,0 +1,75 @@
+// pages/3.13/index.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+
+  },
+
+  async extendPageTest(){
+    this.hi('weapp')
+    // 使用request3
+    let res4 = await wx.wxp.request4({
+      url: `${getApp().globalData.apiUrl}/user/home`,
+    })
+    if (res4) console.log('res4', res4)
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad(options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload() {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh() {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom() {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage() {
+
+  }
+})
--- miniprogram/class3/pages/index/index.js
+++ miniprogram/class3/pages/index/index.js
@@ -10,6 +10,7 @@ Page({
         { index: "/pages/3-10/custom/index", name: "原生tabBar组件-跳转", content: "", type: "navigate" },
         { index: "/pages/3-10/index", name: "自定义tabBar组件", content: "基于系统提供的自定义方式，实现一个tabBar组件。app.json文件中tabBar节点custom设置为ture", type: "switchTab" },
         { index: "/pages/3.12/index", name: "自定义组件拓展", content: "给任意组件添加通用方法", type: "navigate" },
+        { index: "/pages/3.13/index", name: "开放接口", content: "如何对Page进行全局扩展", type: "navigate" },
       ]
     }
   }, 
